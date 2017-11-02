088c42f23da154dd299a03a06d42660b9ea916cf
Allan Tenenbaum <tenenbaumallan@gmail.com>
2017-11-02 12:25:34 -0400

    Fin

---
+++ forScript.js
@@ -0,0 +1,12 @@
+function initialize(){
+    writeCount = 0;
+}
+
+function newIteration(){
+    for (i=0; i<1000; i++){
+        writeCount = writeCount + 1;
+        String(writeLine);
+        newLine = writeLine
+        write_box.innerHTML = newLine;
+    }
+}
